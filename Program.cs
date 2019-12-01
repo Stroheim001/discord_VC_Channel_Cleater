48a1f5384dd819b527e73997ee738eb3eb830fa4
佐竹涼太 <picoreamosaiko@docomo.ne.jp>
2019-12-01 13:37:51 +0900

    itinial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VC_Channel_Creater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
