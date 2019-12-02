0b5262c59f0453e9d8fe3c344f44686c7d95e278
佐竹涼太 <picoreamosaiko@docomo.ne.jp>
2019-12-03 04:25:18 +0900

    名前の変更とコメントの追加

--- command.cs
+++ command.cs
@@ -6,15 +6,25 @@ using Discord.WebSocket;
 
 using VoiceChannelCreater;
 
-public class Dice : ModuleBase
+public class CommandList : ModuleBase
 {
-
+    /// <summary>
+    /// 人数制限
+    /// </summary>
+    /// <param name="limit">制限する人数</param>
+    /// <returns>Task</returns>
     [Command("limit")]
     public async Task userLimit(int limit)
     {
         SocketVoiceChannel channel = (SocketVoiceChannel)Bot.discord.GetChannel(Bot.channel_list[Bot.discord.CurrentUser.Id]);
         await channel.ModifyAsync(x => x.UserLimit = limit);
     }
+
+    /// <summary>
+    /// 名前の変更
+    /// </summary>
+    /// <param name="name">変更する名前</param>
+    /// <returns>Task</returns>
     [Command("name")]
     public async Task nameChange(string name)
     {
